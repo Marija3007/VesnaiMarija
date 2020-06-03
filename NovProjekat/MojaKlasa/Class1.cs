67ccf932ffdb977141001b1077c1bc52abb2c9f5
Marija Runjevac <marijarunjavac@gmail.com>
2020-06-03 11:57:55 +0200

    Komit sa klas library

---
+++ NovProjekat/MojaKlasa/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MojaKlasa
+{
+    public class Class1
+    {
+        private string naziv;
+
+        public string Naziv { get => naziv; set => naziv = value; }
+    }
+}
